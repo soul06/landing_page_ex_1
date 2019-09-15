4bf076244c1b5adf783bec01f2e60ec0d25d2cd0
Anh <continuereading045@gmail.com>
2019-09-15 11:33:40 +0700

    Init project

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <LandingPage msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LandingPage from '@/components/LandingPage.vue'
+
+export default {
+  name: 'home',
+  components: {
+    LandingPage
+  }
+}
+</script>
